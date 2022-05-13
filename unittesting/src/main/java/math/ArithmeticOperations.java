5bbbefa8aab01838d1de2e7bf5a44b50fcbb9277
Papajohn77 <t8190314@aueb.gr>
2022-05-13 11:46:01 +0300

    Add test cases for ArithmeticOperations Class

--- unittesting/src/main/java/math/ArithmeticOperations.java
+++ unittesting/src/main/java/math/ArithmeticOperations.java
@@ -3,13 +3,9 @@ package math;
 /**
 * The MathOperations provides simple arithmetic operations
 * that serve as hands-on practice on Unit Testing.
-*
-* @author  agkortzis
-* @version 1.0
-* @since   2020-04-06 
 */
 public class ArithmeticOperations {
-	
+
 	/**
 	 * Performs the basic arithmetic operation of division.
 	 * @param numerator the numerator of the operation
@@ -19,14 +15,13 @@ public class ArithmeticOperations {
 	 */
 	public double divide(double numerator, double denominator) {
 		if (denominator == 0) throw new ArithmeticException("Cannot divide with zero");
-		
+
 		return numerator/denominator;
 	}
-	
-	
+
 	/**
 	 * Performs the basic arithmetic operation of multiplication 
-	 * between two positive Integers
+	 * between two non negative Integers
 	 * @param x the first input
 	 * @param y the second input
 	 * @return the product of the multiplication
@@ -36,12 +31,12 @@ public class ArithmeticOperations {
 	public int multiply(int x, int y) {
 		if (x < 0 || y < 0) {
 			throw new IllegalArgumentException("x & y should be >= 0");
-		} else if (x <= Integer.MAX_VALUE/y) {
-			return x*y;			
-		} else {
-			throw new IllegalArgumentException("The product does not fit in an Integer variable");		
 		}
+
+		if (y != 0 && x > Integer.MAX_VALUE/y) {
+			throw new IllegalArgumentException("The product does not fit in an Integer variable");			
+		}
+
+		return x*y;	
 	}
-	
-	
 }
