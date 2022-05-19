f5bd148f4c16893b9fa8c18b2d14c4c854f1505d
Papajohn77 <t8190314@aueb.gr>
2022-05-19 19:05:09 +0300

    Create isPrime method in the MyMath Class & its test cases

--- unittesting/src/main/java/math/MyMath.java
+++ unittesting/src/main/java/math/MyMath.java
@@ -1,5 +1,9 @@
 package math;
 
+/**
+* The MyMath provides simple methematical operations
+* that serve as hands-on practice on Unit Testing.
+*/
 public class MyMath {
 
     /**
@@ -18,5 +22,27 @@ public class MyMath {
         }
 
         return n * factorial(n - 1);
+    }
+
+    /**
+	 * Checks if the given integer number is a prime number.
+	 * @param n the number we want to check if it is prime
+	 * @return true if n is a prime number else false
+	 * @exception IllegalArgumentException when n < 2
+	 */
+    public boolean isPrime(int n) {
+        if (n < 2) {
+            throw new IllegalArgumentException("n should be >= 2");
+        } else if (n == 2 || n == 3) {
+            return true;
+        } else if (n % 2 == 0) {
+            return false;
+        }
+
+        for (int i = 3; i < Math.sqrt(n) + 1; i += 2) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
     }
 }
--- unittesting/src/test/java/math/MyMathTest.java
+++ unittesting/src/test/java/math/MyMathTest.java
@@ -30,4 +30,30 @@ public class MyMathTest {
         thrown.expectMessage("n should be 0 <= n <= 12");
         myMath.factorial(13);
     }
+
+    @Test
+    public void test_isPrime_nLessThanTwo() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("n should be >= 2");
+        myMath.isPrime(0);
+    }
+
+    @Test
+    public void test_isPrime() {
+        Assert.assertTrue(myMath.isPrime(2));
+        Assert.assertTrue(myMath.isPrime(3));
+        Assert.assertTrue(myMath.isPrime(5));
+        Assert.assertTrue(myMath.isPrime(7));
+        Assert.assertTrue(myMath.isPrime(11));
+        Assert.assertTrue(myMath.isPrime(107));
+        Assert.assertTrue(myMath.isPrime(7229));
+
+        Assert.assertFalse(myMath.isPrime(4));
+        Assert.assertFalse(myMath.isPrime(6));
+        Assert.assertFalse(myMath.isPrime(8));
+        Assert.assertFalse(myMath.isPrime(9));
+        Assert.assertFalse(myMath.isPrime(77));
+        Assert.assertFalse(myMath.isPrime(729));
+        Assert.assertFalse(myMath.isPrime(7777));
+    }
 }
