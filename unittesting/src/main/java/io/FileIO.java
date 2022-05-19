b7ccc1a5c5f2a2a24ca07efd1fd1b3e1c2ed945d
Papajohn77 <t8190314@aueb.gr>
2022-05-19 17:36:37 +0300

    Add test cases for FileIO Class

--- unittesting/src/main/java/io/FileIO.java
+++ unittesting/src/main/java/io/FileIO.java
@@ -2,7 +2,6 @@ package io;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,13 +10,9 @@ import java.util.List;
 /**
 * The FileIO provides simple file input/output operations
 * that serve as hands-on practice on Unit Testing.
-*
-* @author  agkortzis
-* @version 1.0
-* @since   2020-04-06 
 */
 public class FileIO {
-	
+
 	/**
 	 * Reads a file that contains numbers line by line 
 	 * and returns an array of the integers found in the file.
@@ -25,30 +20,31 @@ public class FileIO {
 	 * @return an array of numbers
 	 * @exception IllegalArgumentException when the given file does not exist
 	 * @exception IllegalArgumentException when the given file is empty
-	 * @exception IOException when an IO interruption occurs (not required to be tested)
+	 * @exception IllegalArgumentException when the given file does not contain only integers
 	 */
 	public int[] readFile(String filepath) {
 		File file = new File(filepath);
 		if (!file.exists()) 
 			throw new IllegalArgumentException("Input file does not exist");
-		
+
 		List<Integer> numbersList = new ArrayList<>();
-		BufferedReader reader;
-		try {
-			reader = new BufferedReader(new FileReader(file));
-			String line = null;
+		try (
+			BufferedReader reader = new BufferedReader(new FileReader(file))
+		) {
+			String line;
 			while ((line = reader.readLine()) != null) {
-				int number = Integer.parseInt(line);
-				numbersList.add(number);
+				numbersList.add(Integer.parseInt(line));
 			}
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(
+				"Input file does not contain only integers");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		if (numbersList.size() == 0) 
-			throw new IllegalArgumentException("Given file is empty");
-		
-		// Convert a List to an array using 
+
+		if (numbersList.size() == 0)
+			throw new IllegalArgumentException("Input file is empty");
+
 		return numbersList.stream().mapToInt(i -> i).toArray();
 	}
 
---
+++ unittesting/src/test/java/io/FileIOTest.java
@@ -0,0 +1,57 @@
+package io;
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+public class FileIOTest {
+    public static String resourcePath = "src/test/resources/";
+    private FileIO fileIO;
+
+    @Before
+    public void setUp() {
+        fileIO = new FileIO();
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void test_readFile_validNumbers() {
+        String filepath = resourcePath.concat("numbers_valid.txt");
+        int[] expectedNumbers = new int[] {
+            7, 7, 2, 2, 9, 9, -7, -7, 0
+        };
+    
+        Assert.assertArrayEquals(expectedNumbers, fileIO.readFile(filepath));
+    }
+
+    @Test
+    public void test_readFile_invalidNumbers() {
+        String filepath = resourcePath.concat("numbers_invalid.txt");
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Input file does not contain only integers");
+    
+        fileIO.readFile(filepath);
+    }
+
+    @Test
+    public void test_readFile_emptyFile() {
+        String filepath = resourcePath.concat("empty.txt");
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Input file is empty");
+    
+        fileIO.readFile(filepath);
+    }
+
+    @Test
+    public void test_readFile_fileNotExist() {
+        String filepath = resourcePath.concat("not_exist.txt");
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Input file does not exist");
+    
+        fileIO.readFile(filepath);
+    }
+}
